e1d53b81ca84a185f669423f86cda8809060d41f
Nguyễn Minh Phương <nguyenminhphuong@shippo.vn>
2018-09-16 20:28:57 +0700

    trang dau tien

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import Login from "./src/components/Signin/Login";
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Login />
+    );
+  }
+}
